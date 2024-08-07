eb114e62d17ff1626494b98372e8846aa9e683b6
Farbod Vand <pharbod95@gmail.com>
2024-08-08 02:16:27 +0330

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+
+export const API_KEY = 'e2c83f19-154c-4e4d-8086-74e48f9b8cc4';
+
+export const TIMEOUT_SEC = 10;
---
+++ src/js/controller.js
@@ -0,0 +1,33 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+// const recipeContainer = document.querySelector('.recipe');
+
+// https://forkify-api.herokuapp.com/v2
+///////////////////////////////////////
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1. Loading the recipe
+    await model.loadRecipe(id);
+
+    // 2. Rendering the recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+controlRecipes();
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+
+init();
